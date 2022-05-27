c05fb401e1397a9110e4d82208d253348d258296
kky04994 <kkyo8747@naver.com>
2022-05-27 22:56:18 +0900

    init

---
+++ src/main/kotlin/com/example/ipsedall/IpsedallApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ipsedall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IpsedallApplication
+
+fun main(args: Array<String>) {
+    runApplication<IpsedallApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ipsedall/IpsedallApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ipsedall
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IpsedallApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
