0e8d84dd7f090587d9f1c98a103baa1bd72e59da
kky04994 <kkyo8747@naver.com>
2022-05-29 17:38:07 +0900

    feat: add SecurityConfig

---
+++ src/main/kotlin/com/example/ipsedall/security/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.example.ipsedall.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http
+            .csrf().disable()
+            .formLogin().disable()
+            .cors()
+            .and()
+            .authorizeRequests()
+            .anyRequest()
+            .permitAll()
+    }
+}
