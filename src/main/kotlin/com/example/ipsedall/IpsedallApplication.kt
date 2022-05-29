33bddee59ec840bd6d1b2542e721534502232071
kky04994 <kkyo8747@naver.com>
2022-05-29 17:32:35 +0900

    feat: add ConfigurationPropertiesConfig

--- src/main/kotlin/com/example/ipsedall/IpsedallApplication.kt
+++ src/main/kotlin/com/example/ipsedall/IpsedallApplication.kt
@@ -1,8 +1,12 @@
 package com.example.ipsedall
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+internal const val BASE_PACKAGE = "com.example.ipsedall"
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class IpsedallApplication
 
